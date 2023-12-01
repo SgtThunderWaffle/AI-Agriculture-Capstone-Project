b55283505a5c1361cb3204563ad722b91f5aa137
SgtThunderWaffle <tulsatinytim@gmail.com>
2023-11-30 23:02:32 -0600

    Changed method of populating csvOut.csv and adjusted column count

---
+++ csvOutGenerator.ps1
@@ -0,0 +1,20 @@
+﻿param (
+    [Parameter(Mandatory=$true)]
+    [string]$ImagesDir,
+    [Parameter(Mandatory=$true)]
+    [string]$TrainCSVPath
+)
+
+$csvTraining = Import-Csv $TrainCSVPath
+$images = Get-ChildItem $ImagesDir -File
+
+$imagesSampled = @()
+while ($imagesSampled.Count -lt 25) {
+    $entry = ($csvTraining | Get-Random).fileName
+    if ($imagesSampled -notcontains $entry) {
+        Copy-Item "$((Get-Item $ImagesDir).FullName)/$entry" "$PSScriptRoot/app/static/images/"
+        $imagesSampled += $entry
+    }
+}
+
+$csvTraining | ?{$imagesSampled -contains $_.fileName} | Export-Csv "$PSScriptRoot/app/csvOut.csv" -Force -NoTypeInformation
