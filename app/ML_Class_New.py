2c03e4230f136b9492266c9b9fd81f291ec13471
PatrickVir <patrickvirata@gmail.com>
2023-12-06 20:06:20 -0600

    Javascript fix on step4labeling.html
    fixed the popover to work when pressed during labeling
    
    
    Former-commit-id: a58c261e0f51998f59cbbd7a79757a826603a410

--- app/ML_Class_New.py
+++ app/ML_Class_New.py
@@ -205,8 +205,10 @@ class ML_Model:
                 y_pic_tail = y_pic_tail[5:]
             for y_idx, y in enumerate(y_actual):
                 if y == 'H':
+                    print(y_idx, len(y_pic_result))
                     health_pic.append(y_pic_result[y_idx])
                 elif y == 'B':
+                    print(y_idx, len(y_pic_result))
                     blight_pic.append(y_pic_result[y_idx])
         return health_pic, blight_pic
 
@@ -245,9 +247,11 @@ class ML_Model:
         blight_pic_prob = []
         for y_idx, y in enumerate(y_pred):
             if y == 'H':
+                print(y_idx, len(y_pic_result))
                 health_pic.append(test_pic[y_idx])
                 health_pic_prob.append(y_prob[y_idx])
             elif y == 'B':
+                print(y_idx, len(y_pic_result))
                 blight_pic.append(test_pic[y_idx])
                 blight_pic_prob.append(y_prob[y_idx])
         health_list = list(zip(health_pic,health_pic_prob))
--- tests/integration/test_ML_Class_New.py
+++ tests/integration/test_ML_Class_New.py
@@ -5,7 +5,9 @@ Created November 2023
 
 import pytest
 import pandas as pd
+import numpy as np
 from app import app
+from app.web import getData
 from app.DataPreprocessing import DataPreprocessing
 from app.ML_Class_New import ML_Model, Active_ML_Model, load_model, tempload_model, generate_token, is_locked
 from app.SamplingMethods import lowestPercentage
@@ -15,51 +17,63 @@ import os
 
 
 
-def test_ml_model_initialization(ml_model):
-    #Given a sample ml_model 
-    #When tested for proper innitialization
-    #Then it will output what was given
-    assert ml_model.ml_classifier.__class__ == RandomForestClassifier().__class__
-    assert ml_model.preprocess is not None
-    assert ml_model.X is not None
-    assert ml_model.Y is not None
-    assert ml_model.get_token() == ml_model.token
 
+def test_ml_model_infoForProgress():
+    # Given an ml_model with practical data
+    # Assuming the labeling is either H for healthy or B for blighted 
+    # Then itll return the current images listed as H and B
+    ml_classifier = RandomForestClassifier() 
+    preprocess = DataPreprocessing(True) 
+    data = getData()
+    token = generate_token()
+    modeldir = 'Models/'
+    tempdir = 'tempdata/'
 
+    ml_model = ML_Model(ml_classifier, preprocess, data, token, modeldir, tempdir)
 
-def test_locking_mechanism(ml_model):
-    #Given a sample ml_model
-    #When testing whether its locked or not
-    #Then it will be locked if temp data exist and it is not locked if tempdata does not exist
-    ml_model.tempsave_model()
-    assert is_locked(ml_model.tempdir, ml_model.get_token())  # Model is locked after tempsave_model()
+    ml_model = ml_model.infoForProgress()
 
-    # clear_tempdata() works in practice, but not in test 
-    # ml_model.clear_tempdata()
-    # assert not is_locked(ml_model.tempdir, ml_model.get_token()) 
-    
+    health_pic, blight_pic = ml_model
+    assert len(health_pic) == 2  # Assuming two 'H' based on ml_model fixture
+    assert len(blight_pic) == 1  # Assuming one 'B' based on ml_model fixture
 
+def test_infoForResults(active_ml_model):
+    # Set up test data (you might need to customize this based on your actual data)
+    test_data = pd.DataFrame({
+        'Feature1': [1, 2, 3, 2, 3, 2],
+        'Feature2': [4, 5, 6, 2, 3, 2],
+        'Feature3': [4, 5, 6, 2, 3, 2],
+        'Feature4': [4, 5, 6, 2, 3, 2],
+        'Feature5': [4, 5, 6, 2, 3, 2],
+        'Feature6': [4, 5, 6, 2, 3, 2],
+        'Feature7': [4, 5, 6, 2, 3, 2],
+        'Feature8': [4, 5, 6, 2, 3, 2],
+        'Feature9': [4, 5, 6, 2, 3, 2],
+        'Feature10': [4, 5, 6, 2, 3, 2],
+        'Feature11': [4, 5, 6, 2, 3, 2],
+        'Feature12': [4, 5, 6, 2, 3, 2],
+        'Feature13': [4, 5, 6, 2, 3, 2],
+        'Feature14': [4, 5, 6, 2, 3, 2],
+        'Feature15': [4, 5, 6, 2, 3, 2],
+        'Feature16': [4, 5, 6, 2, 3, 2],
+        'Feature17': [4, 5, 6, 2, 3, 2],
+        'Feature18': [4, 5, 6, 2, 3, 2],
+        'Feature19': [4, 5, 6, 2, 3, 2],
+        'Feature20': [4, 5, 6, 2, 3, 2],
 
-def test_model_saving_loading(ml_model):
-    # Given a Saved Model
-    # When the model is loaded back in
-    # Then it will be able to load the model back in
-    ml_model.save_model()
-    loaded_model = load_model(ml_model.modeldir, ml_model.tempdir, ml_model.get_token())
-    assert loaded_model is not None
-    assert loaded_model.ml_classifier == ml_model.ml_classifier
-    assert loaded_model.preprocess == ml_model.preprocess
-    assert loaded_model.X.equals(ml_model.X)
-    assert loaded_model.Y.equals(ml_model.Y)
-    assert loaded_model.get_token() == ml_model.get_token()
+        'Label': ['H', 'B', 'H','H', 'B', 'H']
+    })
 
+    # Assuming ml_model.train_files, ml_model.ml_model, ml_model.GetUnknownPredictions, etc. are set up correctly
 
-def test_active_ml_model_initialization(active_ml_model):
-    #Given a sample ml_model 
-    #When tested for proper innitialization
-    #Then it will output what was given
-    sample_size = 10
-    assert len(active_ml_model.sample) == sample_size
-    assert len(active_ml_model.test) == sample_size - len(active_ml_model.sample)
+    # Call the function being tested
+    health_pic_user, blight_pic_user, new_health_pic, new_blight_pic, new_health_pic_prob, new_blight_pic_prob = active_ml_model.infoForResults(test_data)
 
+    # Make assertions based on your expectations
+    assert isinstance(health_pic_user, list)
+    assert isinstance(blight_pic_user, list)
+    assert isinstance(new_health_pic, list)
+    assert isinstance(new_blight_pic, list)
+    assert isinstance(new_health_pic_prob, list)
+    assert isinstance(new_blight_pic_prob, list)
 
